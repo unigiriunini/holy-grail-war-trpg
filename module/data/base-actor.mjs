d2fdd6e695ea150da2b8e5fc0d2c6f53bb7cfdf0
Unigiri Unini <me@unigiri.net>
2024-12-18 06:32:55 +0900

    Define all fields in BaseActor Data Model

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -18,6 +18,42 @@ export default class MyFVTTSystemActorBase extends MyFVTTSystemDataModel {
     schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
     schema.appearance = new fields.StringField({ required: true, blank: true });
+    schema.note = new fields.StringField({ required: true, blank: true });
+    schema.sex = new fields.StringField({ required: true, blank: true });
+    schema.law_versus_chaos_alignment = new fields.StringField({ required: true, blank: true });
+    schema.good_versus_evil_alignment = new fields.StringField({ required: true, blank: true });
+    schema.status = new fields.SchemaField({
+        strength: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        }),
+        constitution: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        }),
+        dexterity: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        }),
+        mana: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        }),
+        luck: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        }),
+        noble_phantasm: new fields.SchemaField({
+            value: new fields.StringField({ required: true, blank: true }),
+            plus: new fields.StringField({ required: true, blank: true }),
+            ex: new fields.StringField({ required: true, blank: true })
+        })
+    });
 
     return schema;
   }
